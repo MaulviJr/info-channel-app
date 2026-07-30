447483dddc59168b9c0f9a8d5ad29c242aa498cc
MaulviJr <abdulhadighouri123@gmail.com>
2026-07-30 19:58:18 +0500

    env. fix in axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.VITE_API_URL,
   // timeout: 5000,
 //   headers: { "X-Custom-Header": "foobar" },
     withCredentials: true,
